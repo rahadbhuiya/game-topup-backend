ed6366e0aec5a3c1a505147310ab6bfb04f0b481
Rahad bhuiya <62468406+rahadbhuiya@users.noreply.github.com>
2025-04-21 00:57:37 +0600

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
 
+const orderSchema = new mongoose.Schema({
+  game: String,
+  playerId: String,
+  package: String,
+  paymentMethod: String,
+  transactionId: String,
+  status: { type: String, default: 'Pending' },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Order', orderSchema);
